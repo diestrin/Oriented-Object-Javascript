1c7aea1cfa00bce92da6393177371a7aaa85eaf3
Diego Barahona <diestrin@gmail.com>
2012-07-17 12:36:36 -0600

    Setting Public function out of main function

--- com/diegobarahona/Mamifero.js
+++ com/diegobarahona/Mamifero.js
@@ -1,9 +1,4 @@
-window.Mamifero = function(){
-
-	/*CONSTRUCT*/ function init(){
-
-	}
-
+(function(){
 	/*PRIVATE*/ function mover(direccion , velocidad){
 		switch(direccion){
 			case "x":
@@ -19,25 +14,32 @@ window.Mamifero = function(){
 				break;
 		}
 	}
-
-	//Las constantes se declaran con doble guion bajo al inicio y al final, son constantes simuladas
-
-	/*PUBLIC*/ this.__reproduccion__ = "sexual";
-	/*PUBLIC*/ this.__nacimiento__ = "parto";
-
-	/*PUBLIC*/ this.x = 0;
-	/*PUBLIC*/ this.y = 0;
-
-	/*PUBLIC*/ this.nombre = "Mamifero";
-	/*PUBLIC*/ this.color_de_piel = "ffffff";
-
-	/*PUBLIC*/ this.caminar = function(direccion){
-		mover.call(direccion , 5);
-	}
-
-	/*PUBLIC*/ this.correr = function(direccion){
-		mover.call(direccion , 10);
+	
+	window.Mamifero = function(){
+	
+		/*CONSTRUCT*/ function init(){
+	
+		}
+	
+		//Las constantes se declaran con doble guion bajo al inicio y al final, son constantes simuladas
+	
+		/*PUBLIC*/ this.__reproduccion__ = "sexual";
+		/*PUBLIC*/ this.__nacimiento__ = "parto";
+	
+		/*PUBLIC*/ this.x = 0;
+		/*PUBLIC*/ this.y = 0;
+	
+		/*PUBLIC*/ this.nombre = "Mamifero";
+		/*PUBLIC*/ this.color_de_piel = "ffffff";
+		
+		return init(); //Ejecuta el constructor
 	}
 	
-	return init(); //Ejecuta el constructor
-}
+	/*PUBLIC*/ Mamifero.prototype.caminar = function(direccion){
+		mover.call(direccion , 5);
+	}
+	
+	/*PUBLIC*/ Mamifero.prototype.correr = function(direccion){
+		mover.call(direccion , 10);
+	}
+})();
